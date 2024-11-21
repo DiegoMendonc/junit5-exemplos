9ce1ff888133c6ab6d03f9b00c80260863f789af
DiegoMendonc <diego_felipe_g.m@live.com>
2024-11-21 12:48:14 -0300

    Update CondicionaisTeste.java
    Aula 05

--- src/test/java/com/github/willyancaetano/junit/CondicionaisTeste.java
+++ src/test/java/com/github/willyancaetano/junit/CondicionaisTeste.java
@@ -27,6 +27,8 @@ package com.github.willyancaetano.junit;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledForJreRange;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 import static org.junit.jupiter.api.condition.JRE.JAVA_11;
 import static org.junit.jupiter.api.condition.JRE.JAVA_17;
